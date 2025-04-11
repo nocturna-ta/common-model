da855bc2b44b5d9578f0174561a0197ac3cf9216
Ayyasythz <bbyasyi@gmail.com>
2025-04-11 13:51:49 +0700

    added kpu kota, provinsi message

--- models/event/blockchain.go
+++ models/event/blockchain.go
@@ -1,11 +1,19 @@
 package event
 
-type KPUBranchMessage struct {
+type KPUKotaMessage struct {
 	BaseModelMessage
-	ID            string `json:"id,omitempty"`
-	Name          string `json:"name,omitempty"`
-	BranchAddress string `json:"branch_address,omitempty"`
-	Region        string `json:"region,omitempty"`
+	ID      string `json:"id,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Address string `json:"address,omitempty"`
+	Region  string `json:"region,omitempty"`
+}
+
+type KPUProvinsiMessage struct {
+	BaseModelMessage
+	ID      string `json:"id,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Address string `json:"address,omitempty"`
+	Region  string `json:"region,omitempty"`
 }
 
 type VoterMessage struct {
