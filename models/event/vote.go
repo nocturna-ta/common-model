f2e61316ea9b9e5b8be2d23ed6ea19058480eb59
Ayyasythz <bbyasyi@gmail.com>
2025-05-20 22:16:23 +0700

    vote message model

--- models/event/vote.go
+++ models/event/vote.go
@@ -1,10 +1,21 @@
 package event
 
-type VoteMessage struct {
+import "time"
+
+type VoteSubmitMessage struct {
 	BaseModelMessage
-	ID             string `json:"id,omitempty"`
-	VoterID        string `json:"voter_id,omitempty"`
-	KTP            string `json:"ktp,omitempty"`
-	ElectionPairID string `json:"election_pair_id,omitempty"`
-	Timestamp      string `json:"timestamp,omitempty"`
+	VoteID            string    `json:"vote_id,omitempty"`
+	VoterID           string    `json:"voter_id,omitempty"`
+	ElectionPairID    string    `json:"election_pair_id,omitempty"`
+	Region            string    `json:"region,omitempty"`
+	SignedTransaction string    `json:"signed_transaction,omitempty"`
+	SubmittedAt       time.Time `json:"submitted_at,omitempty"`
+}
+
+type VoteProcessedMessage struct {
+	VoteID          string    `json:"vote_id,omitempty"`
+	Status          string    `json:"status,omitempty"`
+	TransactionHash string    `json:"transaction_hash,omitempty"`
+	ErrorMessage    string    `json:"error_message,omitempty"`
+	ProcessedAt     time.Time `json:"processed_at,omitempty"`
 }
