887a102553d87cdefc32cde83ae34d3070490cd6
Ayyasythz <bbyasyi@gmail.com>
2025-06-10 23:27:16 +0700

    add users logs

--- models/logging/users.go
+++ models/logging/users.go
@@ -4,8 +4,8 @@ import "github.com/nocturna-ta/common-model/models/event"
 
 type KPULogs struct {
 	event.BaseModelMessage
-	Name     string `json:"name,omitempty"`
-	KPUName  string `json:"kpu_name,omitempty"`
-	KPUType  string `json:"kpu_type,omitempty"`
+	UserID   string `json:"user_id,omitempty"`
+	Address  string `json:"address,omitempty"`
+	Role     string `json:"role,omitempty"`
 	Activity string `json:"activity,omitempty"`
 }
